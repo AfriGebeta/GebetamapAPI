70cae7e2d08b785fd19da7fc5e9c97c4e944a91d
Abenezer Seifu <57215372+abenezer121@users.noreply.github.com>
2022-08-24 13:38:32 +0300

    fixes

--- src/data/responsecode.js
+++ src/data/responsecode.js
@@ -7,4 +7,50 @@ export const direction =
         <span className='ml-[20%]'>// handle the error</span>,
         <span className='ml-[10%]'>&#125;);</span>
     ]
- 
+
+
+export const responseSampleForDirection200 = [
+    <span>&#123;</span>,
+
+                <span className='pl-[3%]'>totalDistance:471,</span>,
+                <span className='pl-[3%]'>code:ok,</span> ,
+                <span className='pl-[3%]'>"direction":[</span>  ,  
+                <span className='pl-[6%]'>&#123; 9.2132 , 36.1232 &#125;,</span>,
+                <span className='pl-[6%]'>&#123; 9.2132 , 36.1232 &#125;,</span>,
+                <span className='pl-[3%]'>]</span>,
+                <span className=''>&#125;</span>
+]
+
+export const responseSampleForDirection400 = [
+    <span>&#123;</span>,
+                <span className='px-[3%]'>"hints" :  &#123;</span>,
+                <span className='pl-[6%]'>"visited_nodes.sum": 58,</span>,
+                <span className='pl-[3%]'>&#125;,</span>,
+                <span className='pl-[3%]'>"paths":[</span>  ,  
+                <span className='pl-[6%]'>&#123;4344 , 36.1232 &#125;,</span>,
+                <span className='pl-[6%]'>&#123; 9.2132 , 36.1232 &#125;,</span>,
+                <span className='pl-[6%]'>&#123; 9.2132 , 36.1232 &#125;,</span>,
+                <span className='pl-[6%]'>&#123; 9.2132 , 36.1232 &#125;,</span>,
+                <span className='pl-[6%]'>&#123; 9.2132 , 36.1232 &#125;,</span>,
+                <span className='pl-[6%]'>&#123; 9.2132 , 36.1232 &#125;,</span>,
+                <span className='pl-[3%]'>]</span>,
+                <span className=''>&#125;</span>
+]
+
+export const responseSampleForDirection500 = [
+    <span>&#123;</span>,
+                <span className='px-[3%]'>"hints" :  &#123;</span>,
+                <span className='pl-[6%]'>"visited_nodes.sum": 58,</span>,
+                <span className='pl-[3%]'>&#125;,</span>,
+                <span className='pl-[3%]'>"paths":[</span>  ,  
+                <span className='pl-[6%]'>&#123; 9898 , 36.1232 &#125;,</span>,
+                <span className='pl-[6%]'>&#123; 9.2132 , 36.1232 &#125;,</span>,
+                <span className='pl-[6%]'>&#123; 9.2132 , 36.1232 &#125;,</span>,
+                <span className='pl-[6%]'>&#123; 9.2132 , 36.1232 &#125;,</span>,
+                <span className='pl-[6%]'>&#123; 9.2132 , 36.1232 &#125;,</span>,
+                <span className='pl-[6%]'>&#123; 9.2132 , 36.1232 &#125;,</span>,
+                <span className='pl-[3%]'>]</span>,
+                <span className=''>&#125;</span>
+]
+
+
--- src/pages/Documentation.jsx
+++ src/pages/Documentation.jsx
@@ -2,33 +2,23 @@ import React, { useEffect, useState } from "react";
 
 import RequestSample from "../Components/RequestSample";
 import ResponseSample from "../Components/ResponseSample";
-import { direction } from "../data/responsecode";
+import { direction, responseSampleForDirection200, responseSampleForDirection400, responseSampleForDirection500 } from "../data/responsecode";
+const openstreetmap = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 const Documentation = () => {
   return (
     <>
       <div class="flex w-full">
-        <aside class="h-screen sticky top-[0px] w-[60%] md:w-[50%] lg:w-[15%]  bg-black text-white ">
-          <p className="text-left mx-[10%] mt-[20%]">Gebeta Api</p>
-          <div className="mt-[10%] mx-[10%] text-center text-left">
-            <p className="text-left">Explore our APIs</p>
-            <p className="text-left">Route Optimization API</p>
-            <p className="text-left">Routing API</p>
-            <p className="text-left">Geocoding API</p>
-          </div>
-        </aside>
+       
 
         <main className="ml-[5%] mt-[3%]">
           <p className="text-5xl font-black mb-[5%]">Gebeta Routing API</p>
 
           <p>
-            With the GraphHopper Directions API you can integrate A-to-B route
-            planning, turn-by-turn navigation, route optimization, isochrone
-            calculations and other tools in your application.
+           With the GebetaMaps Directions API you can integrate A-to-B route planning, turn-by-turn navigation, route optimization, isochrone calculations, and other tools in your application.
           </p>
 
           <p>
-            The GraphHopper Directions API consists of the following RESTful web
-            services:
+            The GebetaMaps Directions API consists of the following RESTful web services:
           </p>
           <ul className=" mb-[5%] ml-[1%]">
             <li>Routing API</li>
@@ -39,18 +29,14 @@ const Documentation = () => {
           </ul>
           <p className="font-bold text-3xl mb-[5%]">Explore our APIs</p>
           <p className="text-xl">Get Started</p>
-          <p className="ml-[2%]">1. Sign up for GraphHopper </p>
+          <p className="ml-[2%]">1. Sign up for GebetaMaps </p>
           <p className="ml-[2%]">2. Create an API key </p>
           <p>
-            Each API part has its own documentation. Jump to the desired API
-            part and learn about the API through the given examples and
-            tutorials.{" "}
+            Each API part has its own documentation. Jump to the desired API part and learn about the API through the given examples and tutorials.
+            In addition, for each API there are specific sample requests that you can send via Insomnia or Postman to see what the requests and responses look like.
+            .{" "}
           </p>
-          <p>
-            In addition, for each API there are specific sample requests that
-            you can send via Insomnia or Postman to see what the requests and
-            responses look like.
-          </p>
+         
           <p className="mt-[5%] text-2xl font-black">Postman</p>
           <p className="mt-[5%]">
             To explore our APIs with Postman, follow these steps:{" "}
@@ -58,11 +44,10 @@ const Documentation = () => {
           <div className="ml-[2%]">
             <ol>
               <li>
-                Import our request collections as well as our environment file.{" "}
+                Import our request collections as well as our environment file.
               </li>
               <li>
-                Specify your API key in your environment: "api_key": your API
-                key
+                Specify your API key in your environment: "api_key": your API key
               </li>
               <li>Start exploring</li>
             </ol>
@@ -73,22 +58,17 @@ const Documentation = () => {
             information:
           </p>
           <ol className="ml-[5%]">
-            <li>FAQ</li>
-            <li>Public forum</li>
+            <li>Follow us on Twitter</li>
             <li>Contact us</li>
-            <li>GraphHopper Status Page</li>
           </ol>
 
-          <p>To stay informed about the latest developments, you can:</p>
-          <ol className="ml-[5%]">
-            <li>follow us on twitter,</li>
-            <li>read our blog,</li>
-          </ol>
+          
 
 
-          <p className="mt-[5%] font-bold text-3xl mb-[5%]">
+          <p className="mt-[5%] font-bold text-3xl mb-[1%]">
             GET Route Endpoint
           </p>
+          <p className="mt-[1%]   mb-[5%]">Use this url on your map provider and start routing {openstreetmap}</p>
           <p>
             The GET request is the most simple one: just specify the parameter
             in the URL and you are done. Can be tried directly in every browser.
@@ -109,7 +89,7 @@ const Documentation = () => {
               curl='curl "https://mapapi.gebeta.app/api/v1/route/driving/direction/?la1=" + l1 + "&lo1=" + lo1 + "&la2=" + la2 + "&lo2=" + lo2'
               js={direction}
             />
-            <ResponseSample />
+            <ResponseSample responseCodes200={ responseSampleForDirection200} responseCodes400={responseSampleForDirection400 } responseCodes500={responseSampleForDirection500 }      />
           </div>
           {/*Route Optimization problem*/}
           <p className="mt-[5%] font-bold text-3xl mb-[5%]">
@@ -135,7 +115,7 @@ const Documentation = () => {
            
               js={direction}
             />
-            <ResponseSample />
+            <ResponseSample responseCodes200={ responseSampleForDirection200} responseCodes400={responseSampleForDirection400 } responseCodes500={responseSampleForDirection500 }      />
           </div>
           {/*  */}
 
@@ -143,50 +123,33 @@ const Documentation = () => {
             GET Route Endpoint
           </p>
           <p>
-            The Matrix API is part of the GraphHopper Directions API and with it
-            you can calculate many-to-many distances and times a lot more
-            efficient than calling the Routing API multiple times.
+            The Matrix API is part of the GebetaMaps Directions API and with it, you can calculate many-to-many distances and times a lot more efficient than calling the Routing API multiple times.
+
           </p>
           <p>
-            In the Routing API we support multiple points, so called 'via
-            points', which results in one route being calculated. The Matrix API
-            results in NxM routes, or more precise NxM distances or times being
-            calculated but is a lot faster compared to NxM single requests.
+            In the Routing API, we support multiple points, so called 'via points', which results in one route being calculated. The Matrix API results in NxM routes, or more precise NxM distances or times being calculated but is a lot faster compared to NxM single requests.
+The most simple example is a people trying to decide which bus restoursant is close to her/her instead of using beeline distance she/he can calculate a 1x4 matrix. Or a delivery service often in the need of big NxN matrices to solve vehicle routing problems. For example, the GebetaMaps Route Optimization API uses the Matrix API under the hood to achieve this.
+
+ 
           </p>
-          <p>
-            The most simple example is a tourist trying to decide which pizza is
-            close to her instead of using beeline distance she can calculate a
-            1x4 matrix. Or a delivery service often in the need of big NxN
-            matrices to solve vehicle routing problems. For example the
-            GraphHopper Route Optimization API uses the Matrix API under the
-            hood to achieve this.
-          </p>
+         
           <p>Some other use cases for the Matrix API:</p>
           <ol className="ml-[5%]">
             <li>
-              Logistic problems often pick up many items from and deliver them
-              to many locations.
+              Logistic problems often pick up many items from and deliver them to many locations.
             </li>
             <li>
-              Calculating detours with many possible points in-between and
-              selecting the best e.g. interesting for ridesharing or taxi
-              applications. For this 1-to-many requests are necessary.
+              Calculating detours with many possible points in-between and selecting the best e.g. interesting for ridesharing or taxi applications. For this 1-to-many requests are necessary.
             </li>
             <li>
-              Finding the best tour for a tourist in the need to visit as many
-              points of interests as possible.
+             Finding the best tour for a tourist in the need to visit as many points of interests as possible.
             </li>
           </ol>
           <h3>Description</h3>
           <p>
-            The Matrix API calculates the well known distance-matrix for a set
-            of points, i.e. it calculates all the distances between every point
-            combination. But we do not stop there, we also offer a time-,
-            weight- and route-matrix. The weight-matrix can be used as raw input
-            for e.g. a vehicle routing problem (VRP) and is more precise than a
-            time- or distance-matrix. E.g. for bike routes the actual weight of
-            a route (e.g. the "beauty") is what you want to decide if a route is
-            'better' and not always the taken time or distance.
+            The Matrix API calculates the well known distance-matrix for a set of points, i.e. it calculates all the distances between every point combination. 
+
+
           </p>
           <p>
             A simple illustration for a 3x3 matrix with identical from and to
@@ -223,7 +186,7 @@ const Documentation = () => {
             </tbody>
           </table>
           <h3>GET Matrix Endpoint</h3>
-          <p>With this Matrix Endpoint you submit the points and parameters via URL parameters and is the most convenient as it works out-of-the-box in the browser. If possible you should prefer using the POST Matrix Endpoint that avoids problems with many locations and can also gzip the request. (Note, that all endpoints return gzipped responses).</p>
+          <p>With this Matrix Endpoint you submit the points and parameters via URL parameters and is the most convenient as it works out-of-the-box in the browser.</p>
           {/* code component */}
           <div className="w-full bg-[#263238] text-white mb-[5%] flex flex-col">
             <div className="bg-[#11171a] mx-[5%] mt-[2%] py-[1%]">
@@ -239,7 +202,7 @@ const Documentation = () => {
              curl='curl "https://mapapi.gebeta.app/api/v1/route/driving/direction/?la1=" + l1 + "&lo1=" + lo1 + "&la2=" + la2 + "&lo2=" + lo2'
               js={direction}
             />
-            <ResponseSample />
+            <ResponseSample responseCodes200={ responseSampleForDirection200} responseCodes400={responseSampleForDirection400 } responseCodes500={responseSampleForDirection500 }      />
           </div>
         </main>
       </div>
